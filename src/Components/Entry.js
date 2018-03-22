29874cd881737b4f12efd48a1e81be1d7565dfb7
GavinThomas1192 <gthomas1192@gmail.com>
2018-03-22 10:28:10 -0700

    failed prop type error

--- src/Components/Entry.js
+++ src/Components/Entry.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import App from "./App/App";
 class Entry extends Component {
   render() {
+    const error = console.error;
+    console.error = function(warning, ...args) {
+      if (/(Invalid prop|Failed prop type)/.test(warning)) {
+        throw new Error(warning);
+      }
+      error.apply(console, [warning, ...args]);
+    };
     return (
       <div className="App">
         <App />
